4dc95fa8f70b2ff0a0f0acd644baa7c80af22760
Ildar Ibragimov <iibragimov84@gmail.com>
2016-07-06 19:35:47 +0300

    Добавление разметки блока "logo"

--- includes/topPannel.php
+++ includes/topPannel.php
@@ -5,6 +5,13 @@
 ?>
 <div class="top-pannel">
 	<div class="top-pannel__wrapper">
-
+		<div class="top-pannel__logo block_align_middle">
+            <div class="block__wrapper">
+                <a class="logo link link_block block_align_middle" href="/" target="_self">
+                    <span class="logo__mark block__wrapper icon icon_app icon_size_x24"></span>
+                    <span class="logo__name block__wrapper">Front-end Developer Test Task</span>
+                </a>                
+            </div>
+		</div>
 	</div>
 </div>

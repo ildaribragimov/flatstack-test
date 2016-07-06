9ae01f64ff370747615ef065a9cbc63ca696fb93
Ildar Ibragimov <iibragimov84@gmail.com>
2016-07-06 11:23:18 +0300

    Создание базового HTML-дерева страницы, добавление favicon

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+		<meta name="viewport" content="width=device-width, initial-scale=0.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+		<meta charset="utf-8">
+        <title>Front-end Developer Test Task</title>
+        <link rel="icon" href="/img/system/favicon.ico" type="image/x-icon">
+    </head>
+    <body></body>
+</html>

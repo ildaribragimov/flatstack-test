f93e06e34815b55e8be6621ed162601bd18dae4a
Ildar Ibragimov <iibragimov84@gmail.com>
2016-07-07 09:19:41 +0300

    Добавление блока "menu"

--- includes/topPannel.php
+++ includes/topPannel.php
@@ -5,13 +5,11 @@
 ?>
 <div class="top-pannel">
 	<div class="top-pannel__wrapper">
-		<div class="top-pannel__logo block_align_middle">
-            <div class="block__wrapper">
-                <a class="logo link link_block block_align_middle" href="/" target="_self">
-                    <span class="logo__mark block__wrapper icon icon_app icon_size_x24"></span>
-                    <span class="logo__name block__wrapper">Front-end Developer Test Task</span>
-                </a>                
-            </div>
-		</div>
+		<div class="top-pannel__logo block_align_middle"><div class="block__wrapper"><a class="logo link link_block block_align_middle" href="/" target="_self"><span class="logo__mark block__wrapper icon icon_app icon_size_x24"></span><span class="logo__name block__wrapper">Front-end Developer Test Task</span></a></div></div>
+        <div class="top-pannel__menu menu menu_horiz menu_main block_align_middle"><ul class="menu__items block__wrapper">
+            <li class="menu__item">
+                <a class="menu__link link link_block block_align_middle" href="/" target="_self"><span class="menu__item-title block__wrapper">cart</span><span class="menu__item-icon icon icon_cart icon_size_x24 block__wrapper"><span class="menu__item-notification">3</span></a>
+            </li>
+        </ul></div>
 	</div>
 </div>
